80e464427131c7d0712d542e3c12d1b329a8349f
[dyad] <stellardreamzshop@gmail.com>
2025-12-13 18:20:07 -0300

    [dyad] Configuración de analíticas PostHog - wrote 3 file(s), added posthog-js package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Inicialización de PostHog Analytics
+posthog.init('phc_MU1Tv3bAV2xKQNdeDMgv1N9U6prrvWvWu3EJdzKoALM', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only', // Optimización para plan gratuito
+  capture_pageview: false // Se maneja mejor manualmente o automático, por defecto lo dejaremos que capture
+});
+
+createRoot(document.getElementById("root")!).render(
+  <PostHogProvider client={posthog}>
+    <App />
+  </PostHogProvider>
+);
